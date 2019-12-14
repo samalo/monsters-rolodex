6303b30288d7b0d5dfa03ff3a4c1a9e68c67a477
samalo <smhonds@gmail.com>
2019-12-14 18:37:05 +0200

    Updates

---
+++ precache-manifest.9ee0a9d9e0b7c2cbdcc8b43124a14954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cb8a2a5e2d83c5ada3615ca36a8e66",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d1c1fdbc847124fdf9a6",
+    "url": "/monsters-rolodex/static/css/main.ebd40151.chunk.css"
+  },
+  {
+    "revision": "c50fda5d0cde96c1bf93",
+    "url": "/monsters-rolodex/static/js/2.f88b0a3f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.f88b0a3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1c1fdbc847124fdf9a6",
+    "url": "/monsters-rolodex/static/js/main.0df1f2bc.chunk.js"
+  },
+  {
+    "revision": "0851e0b9364926ab074b",
+    "url": "/monsters-rolodex/static/js/runtime-main.46c51de7.js"
+  }
+]);
